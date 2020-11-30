8ffbc3d4a6308cb6c24b1db72cd2a260a2564b2b
defseg <6988161+defseg@users.noreply.github.com>
2020-11-30 07:06:05 -0500

    Fix hayes_prime.json, get rule validation working

--- featuralize.ts
+++ featuralize.ts
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 //  A Feature is the basic element of featuralization.
 //  It has a name and a map of values. This lets us accommodate both UPSID-style descriptive features and PHOIBLE-style
 //  binary features.
@@ -106,7 +108,6 @@ type BaseCharacter = GlyphBase & {
 // Con:
 // - more verbosity
 // - modifiers will surely be specified as arrays of features, so more transformation overheads
-type ModifierRules = Map<Root, Array<Feature>>
 type Modifier = GlyphBase & {
 	klass: "combining" | "spacing" | "prefixal",
 	rules: Map<Array<FeatureValue>, Array<FeatureValue>>
@@ -123,7 +124,6 @@ type Segment = {
 
 class FeatureSchema {
 	raw_schema: Root
-	// TODO: build these in the constructor
 	features_by_name: Map<string, Feature> // map from name of feature to feature
 	features_by_parent: Map<string, FeatureValue> // map from name of feature to {feature: parent, value: branch_to_child}
 
@@ -135,8 +135,10 @@ class FeatureSchema {
 		const build_features_by_name = (f: Feature) => {
 			if (this.features_by_name.has(f.name)) throw new Error(`Ambiguous feature name: ${f.name}`)
 			this.features_by_name.set(f.name, f)
-			for (let cf_k in f.values) {
-				for (let cf of f.values[cf_k]) {
+			for (let cf_value in f.values) {
+				let cf_descendants = f.values[cf_value]
+				console.log(cf_descendants)
+				for (let cf of cf_descendants) {
 					build_features_by_name(cf)
 				}
 			}
@@ -160,35 +162,52 @@ class FeatureSchema {
 	
 	// Get a feature by name.
 	get(name: string) {
-		return this.features_by_name.get(name)
+		const res = this.features_by_name.get(name)
+		if (res === undefined) throw new Error(`Undefined get for ${name}`)
+		return res
+	}
+	get_parent(name: string) {
+		const res = this.features_by_parent.get(name)
+		if (res === undefined) throw new Error(`Undefined get_parent for ${name}`)
+		return res
 	}
 
 	// Ensure that a modifier ruleset is well-formed: the LHS ensures that all RHS features are reachable.
 	// For example, in Hayes-Prime, if the RHS sets [+anterior], the LHS must set [+coronal], because [±anterior] 
 	// is a descendant of [+coronal]: only [+coronal] segments can have the [±anterior] feature.
-	// For a feature value in RHS to be reachable, either:
-	// - the LHS must contain the correct feature of its immediate descendant
-	// - or its feature must be an immediate descendant of root (not "derived")
+	// For a feature value in RHS to be reachable, one of these things must hold:
+	// - its feature must be an immediate descendant of root (not "derived")  (e.g. root -> +long)
+	// - the RHS feature is also the LHS feature                              (e.g. -labiodental -> +labiodental)
+	// - the RHS is a sibling of LHS                                          (e.g. +anterior -> +distributed)
+	// - the LHS must contain the correct feature of its immediate descendant (e.g. +coronal -> +anterior)
+	// This is untested and almost certainly extremely wrong. Need to write parser for rules first.
 	typecheck(modifier: Modifier) {
-		let root_children = new Set(this.raw_schema.values.Features)
-
+		let root_children = new Set(this.raw_schema.values.Features.map(x => x.name))
+ 
 		for (let rule of modifier.rules) {
+			console.log(JSON.stringify(rule))
 			const lhs = rule[0]
 			const rhs = rule[1]
 
 			// Any child of the root feature is always accessible, so we don't need to check.
-			const derived_rhs_features = rhs.filter(x => !root_children.has(x.feature))
+			let derived_rhs_features = rhs.filter(x => !root_children.has(x.feature.name))
+			// If the LHS and RHS are identical, we don't need to check.
+			const lhs_feature_names = new Set(lhs.map(x => x.feature.name))
+			derived_rhs_features = derived_rhs_features.filter(x => !lhs_feature_names.has(x.feature.name))
+			// If the LHS and RHS are siblings (have the same parent and the same descendance value), we don't need to check.
+			const lhs_feature_parents = new Set(lhs.map(x => this.get_parent(x.feature.name)))
+			derived_rhs_features = derived_rhs_features.filter(x =>
+				!lhs_feature_parents.has( this.get_parent(x.feature.name) )
+			)
 
+			// Otherwise, check to see if LHS contains a parent of RHS, with the right branch to RHS.
 			for (let derived_rhs_feature of derived_rhs_features) {
 				const rhs_parent = this.features_by_parent.get(derived_rhs_feature.feature.name)
 				if (rhs_parent === undefined) throw new Error("Undefined RHS parent (this should never happen)")
-				const rhs_value = rhs_parent.value
-
-				const error_msg = Error(`Invalid rule ${rule} for ${modifier.glyph}`)
 				
-				if (!lhs.some(fval => {
+				if (!lhs.some(fval => 
 					fval.feature.name === rhs_parent.feature.name && fval.value === rhs_parent.value
-				})) throw error_msg
+				)) throw new Error(`Invalid rule ${JSON.stringify(rule)} for ${modifier.glyph}`)
 			}
 		}
 	}
@@ -205,26 +224,113 @@ class Featuralizer {
 	}
 
 	// Declare a feature schema, i.e. a specific model, such as PHOIBLE's "Hayes Prime".
-	load_feature_schema(features: Root): void {
+	load_feature_schema(_features: Root): void {
 		
 	}
 
 	// Declare a glyph rule. Insertion order matters: we'll standardize characters by reordering their components.
-	declare_glyph_rule(rule: GlyphRule): void {
+	declare_glyph_rule(_rule: GlyphRule): void {
 
 	}
 
 	// Parse a string into a sequence of glyphs.
-	parse(glyph: string)/*: Array<string> */ {
+	parse(_glyph: string)/*: Array<string> */ {
 
 	}
 
 	// Normalize a glyph - reorder its components.
-	normalize(glyph: Glyph)/*: string */ {
+	normalize(_glyph: Glyph)/*: string */ {
 	}
 
 	// Transform a glyph (a sequence of characters) into a segment (a feature bundle).
-	featuralize(glyph: string)/*: Segment */ {
+	featuralize(_glyph: string)/*: Segment */ {
 		// TODO
 	}
 }
+
+function parse_feature_schema(path: string): FeatureSchema {
+	const raw: any = fs.readFileSync(path)
+	let data: any = JSON.parse(raw)
+
+	const is_binary = true
+	const validate_feature = (feature: any, top_level = true) => {
+		if (!feature.hasOwnProperty('name')) throw new Error('Feature with no name')
+		if (!feature.hasOwnProperty('values')) throw new Error(`Feature with no values: ${feature.name}`)
+		if (!top_level && is_binary && !(
+			Object.keys(feature.values).length === 2 && 
+			feature.values.hasOwnProperty('+') && feature.values.hasOwnProperty('-'))
+		) throw new Error(`Non-binary feature: ${feature.name}`)
+		let descendants: any[] = []
+		for (let v in feature.values) descendants = descendants.concat(feature.values[v])
+		descendants.map(f => validate_feature(f, false))
+	}
+	data.map((f: any) => validate_feature(f))
+
+	const root: Root = {
+		name: 'Root',
+		values: {
+			'Features': data as Feature[]
+		}
+	}
+
+	return new FeatureSchema(root)
+}
+
+// test stuff
+const schema = parse_feature_schema('featuresets/hayes_prime.json')
+
+// This should work.
+const rules = new Map([
+	[ 
+		[ 
+			{feature: schema.get('coronal') as Feature, value: '+'} as FeatureValue
+		],
+	  	[
+	  		{feature: schema.get('anterior') as Feature, value: '+'} as FeatureValue
+	  	]
+	],
+	[
+		[ 
+			{feature: schema.get('labiodental') as Feature, value: '-'} as FeatureValue
+		],
+		[ 
+			{feature: schema.get('labiodental') as Feature, value: '+'} as FeatureValue
+		]
+	],
+	[
+		[
+			{feature: schema.get('spread_glottis') as Feature, value: '+'} as FeatureValue
+		], 
+		[
+			{feature: schema.get('long') as Feature, value: '+'} as FeatureValue
+		]
+	],
+	[
+		[
+			{feature: schema.get('anterior') as Feature, value: '+'} as FeatureValue
+		],
+		[
+			{feature: schema.get('distributed') as Feature, value: '+'} as FeatureValue
+		]
+	]
+])
+schema.typecheck({
+  klass: "combining",
+  glyph: "test",
+  rules: rules
+})
+
+console.log(' ---- everything that should work works ---- ')
+
+// This should *not* work.
+schema.typecheck({
+	klass: "combining", glyph: "test2",
+	rules: new Map([[
+		[ 
+			{ feature: schema.get('coronal'), value: '-' } 
+		],
+		[
+			{ feature: schema.get('distributed'), value: '+' }
+		]
+	]])
+})
